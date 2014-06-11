8dc80ad41781fb716c38250a93ad9c0d5bd24ece
adam cao <xqcaomail@gmail.com>
2014-06-11 19:38:30 -0400

    1st commit

---
+++ EmailFind.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailFind {
+
+	public static void main(String[] args) {
+		try {
+			//BufferedReader br = new BufferedReader(new FileReader("E:\\emailaa.htm"));
+			BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\li\\Desktop\\abc.txt"));
+			
+			String line = "";
+			while((line=br.readLine()) != null) {
+				parse(line);
+			}
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+
+
+	}
+	private static void parse(String line) {
+		Pattern p = Pattern.compile("[\\w[.-]]+@[\\w[.-]]+\\.[\\w]+");
+		Matcher m = p.matcher(line);
+		while(m.find()) {
+			System.out.println(m.group());
+			
+		}
+	}
+	
+}
